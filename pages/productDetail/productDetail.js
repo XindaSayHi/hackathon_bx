ea02682411fb2ec664cbf591d671c5d55077afd0
makaiqin <makaiqin@baidu.com>
2020-11-25 15:51:21 +0800

    feat:产品详细信息页面开发.

--- pages/productDetail/productDetail.js
+++ pages/productDetail/productDetail.js
@@ -2,31 +2,13 @@ import {bxlist} from '../../data/bxlist';
 
 Page({
     data: {
-        headerList: {
-            title: '四个筛选',
-            selections: [
-                {
-                    text: '种类',
-                    value: '1'
-                },
-                {
-                    text: '公司',
-                    value: '2'
-                },
-                {
-                    text: '年龄',
-                    value: '3'
-                },
-                {
-                    text: '特色',
-                    value: '4'
-                }
-            ]
-        },
-        bxlist: bxlist,
+        bxlist: bxlist[0],
+        canProtectData: [],
+        cantProtectData: [],
     },
     onLoad: function () {
         // 监听页面加载的生命周期函数
+
     },
     onReady: function() {
         // 监听页面初次渲染完成的生命周期函数
@@ -49,10 +31,4 @@ Page({
     onShareAppMessage: function () {
         // 用户点击右上角转发
     },
-    navigateTo(e) {
-        console.log('navigateTo:',e)
-        swan.navigateTo({
-            url: 'pages/productDetail/productDetail'
-        });
-    }
 });
